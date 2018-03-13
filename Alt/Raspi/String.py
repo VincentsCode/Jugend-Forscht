35ed72724b52e03fcb737827adb13d923e455786
VincentsCode <vincent.schmandt@gmx.de>
2018-03-13 21:39:13 +0100

    Cleaned

--- Alt/Raspi/String.py
+++
@@ -1,7 +0,0 @@
-data = "1200,200"
-breakpoint = data.index(",")
-angle = data[0:breakpoint]
-distance = data[breakpoint +1:len(data)]
-
-print "Angle: " + angle + "\n"
-print "Distance: " + distance + "\n"
--- EV3/zu Prototyp 6/jNeu/src/jNeu/jNeu.java
+++
@@ -1,7 +0,0 @@
-package jNeu;
-
-public class jNeu {
-	public static void main(String[] args) {
-		new Robot();
-	}
-}
